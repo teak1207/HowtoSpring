0524ad428120f75d4580a4e29ad2bacb6efe7706
teak1207 <teak1207@gmail.com>
2021-10-26 17:01:18 +0900

    test

--- src/main/resources/com/springbook/biz/common/test.java
+++ src/main/resources/com/springbook/biz/common/test.java
@@ -6,5 +6,6 @@ public class test {
 	public static void main(String[] args) {
 			System.out.println("test123");
 			System.out.println("SSIBAL SSAGGI");
+			System.out.println("fuckfuckfuckfuckfuck");
 	}
 }
